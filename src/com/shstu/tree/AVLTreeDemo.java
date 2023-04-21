cf05e3122ada475e05394c558db31c53a465f2e6
coderssl <3013141211@qq.com>
2023-04-21 18:40:17 +0800

    Commit Message

--- src/com/shstu/tree/AVLTreeDemo.java
+++ src/com/shstu/tree/AVLTreeDemo.java
@@ -25,6 +25,7 @@ public class AVLTreeDemo {
         System.out.println();
         System.out.println("AVL树的高度是：" + avlTree.height() + " 左子树的高度是：" + avlTree.leftHeight() + " 右子树的高度是：" + avlTree.rightHeight());
         System.out.println("根节点是：" + avlTree.getRoot());
+        System.out.println("根节点是：" + avlTree.getRoot());
     }
 }
 
